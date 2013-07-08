e954b6c2c3baeb0088d7d5a9243c0c1520764da4
Corey Chivers <corey.chivers@mail.mcgill.ca>
2013-07-08 02:03:05 +0000

    modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  var indexBuf = fs.readFileSync('index.html');
+  var indexString = indexBuf.toString('ascii');
+  response.send(indexString);
 });
 
 var port = process.env.PORT || 5000;
