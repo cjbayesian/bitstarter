4bcd262f7f01365fc5894b0cd694ac6afc587d40
Corey Chivers <corey.chivers@mail.mcgill.ca>
2013-07-08 17:14:59 +0000

    utf-8 encoding of index.html buffer.

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var indexBuf = fs.readFileSync('index.html');
-  var indexString = indexBuf.toString('ascii');
+  var indexString = indexBuf.toString('utf-8');
   response.send(indexString);
 });
 
